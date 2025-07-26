a9018c34ff68d093fc3d5496d7dbd4cdfe69ab85
ErickQuizhpe <equizhpe2003@gmail.com>
2025-07-26 03:05:46 -0500

    feat: Add mapping for roles in UserResponse conversion

--- src/main/java/com/api/estudiantes/utils/mapper/UserMapper.java
+++ src/main/java/com/api/estudiantes/utils/mapper/UserMapper.java
@@ -1,6 +1,8 @@
 package com.api.estudiantes.utils.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
 import com.api.estudiantes.controller.dto.user.UserResponse;
 import com.api.estudiantes.entity.user.RoleEntity;
 import com.api.estudiantes.entity.user.UserEntity;
@@ -8,7 +10,8 @@ import com.api.estudiantes.entity.user.UserEntity;
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
-  public abstract UserResponse toUserResponse(UserEntity userEntity);
+  @Mapping(target = "roles", source = "roles")
+  UserResponse toUserResponse(UserEntity userEntity);
 
   default String map(RoleEntity roleEntity) {
     return roleEntity.getName().name();
